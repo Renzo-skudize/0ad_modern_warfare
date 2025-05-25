6715196b06f96bcb0a7af3770100d24bf0f1b659
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2025-05-25 17:47:54 +0200

    Create dogfight.js

---
+++ gui/pregame/backgrounds/dogfight.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "dogfight",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "dogfight",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "dogfight",
+      "tiling": false,
+    },
+  ]);
