465bf878537f1566f95436d07b4f03d4247b4fe0
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2025-05-25 17:48:59 +0200

    Create cocked_pistol.js

---
+++ gui/pregame/backgrounds/cocked_pistol.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "cocked_pistol",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "cocked_pistol",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "cocked_pistol",
+      "tiling": false,
+    },
+  ]);
