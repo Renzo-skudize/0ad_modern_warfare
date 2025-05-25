1b85db19bfc43fcc40b1e011e8b3d4eb32c755b0
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2025-05-25 17:46:10 +0200

    Create submarine.js

---
+++ gui/pregame/backgrounds/submarine.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "submarine",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "submarine",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "submarine",
+      "tiling": false,
+    },
+  ]);
