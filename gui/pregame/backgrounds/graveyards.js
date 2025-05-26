e9d7b20d9df260a1bcbf40ea9dc2a5789691570c
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2025-05-26 17:58:03 +0200

    Create graveyards.js

---
+++ gui/pregame/backgrounds/graveyards.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "graveyards",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "graveyards",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "graveyards",
+      "tiling": false,
+    },
+  ]);
