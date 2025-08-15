c1c464c42ab79bb37bf85a37e4b75401a1e346c5
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2025-08-15 13:25:01 +0200

    Update approach.js

--- gui/pregame/backgrounds/approach.js
+++ gui/pregame/backgrounds/approach.js
@@ -1,7 +1,7 @@
 g_BackgroundLayerData.push(
   [
     {
-      "offset": (time, width) => 0.04 * width * Math.cos(0.05 * time),
+      "offset": (time, width) => 0.04 * width * Math.sin(0.05 * time),
       "sprite": "approach",
       "tiling": false,
     },
