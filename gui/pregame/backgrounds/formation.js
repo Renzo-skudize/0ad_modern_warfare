c29b67741289236b0fbb3d9ce7e1c4d426f88c58
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2025-08-15 13:33:05 +0200

    Update formation.js

--- gui/pregame/backgrounds/formation.js
+++ gui/pregame/backgrounds/formation.js
@@ -1,1 +1,8 @@
-
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0.04 * width * Math.sin(0.05 * time),
+      "sprite": "formation",
+      "tiling": false,
+    },
+  ]);
